0e5af8258aad6d449498f2f9ef5b2f0c8d902d5a
Mehdi MECHERI <mmecheri@ONP.local>
2018-05-31 13:31:05 +0200

    clean

--- src/app/modules/cool-notifier/cool-notifier.module.ts
+++ src/app/modules/cool-notifier/cool-notifier.module.ts
@@ -6,6 +6,14 @@ import { CoolNotifierService } from './services/cool-notifier.service';
 import { CoolNotifierComponent } from './components/cool-notifier.component';
 import { NotificationComponent } from './components/notification.component';
 
+export * from './components/notification.component';
+export * from './components/cool-notifier.component';
+export * from './services/cool-notifier.service';
+export * from './interfaces/icons';
+export * from './interfaces/notification';
+export * from './interfaces/options';
+export * from './constants';
+
 @NgModule({
   imports: [
     CommonModule,
